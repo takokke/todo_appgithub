8fbb83cc05e1f69f48cdf73cae380157b762f209
takumi <kotetsuman.0707@gmail.com>
2023-07-01 17:50:29 +0900

    ¡ッセージ

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(function () {
-    
+
     const btn = $("#addTodo")
     btn.click(function() {
         const todo_task = $("#input")
@@ -7,4 +7,17 @@ $(function () {
         todo_task.val("")
     })
 
+    // $("input[type=checkbox]").on("change", function () {
+    //     if($(this).is(":checked")) {
+    //         $(this).parent().css("text-decoration", "line-through")
+    //     }
+    // })
+
+    $(document).on("change", "input[type=checkbox]", function() {
+        if ($(this).is(":checked")) {
+            $(this).parent().css("text-decoration", "line-through")
+        } else {
+            $(this).parent().css("text-decoration", "none")
+        }
+    })
 })
